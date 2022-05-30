6d001206f792c52a9fa80aa56affa7750d63a97c
KishanAcademic <est.kristyam.capera@unimilitar.edu.co>
2022-05-30 10:44:40 -0500

    CRUD completa

--- src/Prenda.php
+++ src/Prenda.php
@@ -38,19 +38,66 @@ class Prenda{
 
     }
     
-    public function actualizar(){
-        
+    public function actualizar($_params){
+        $sql = "UPDATE `prenda` SET `titulo`=:titulo,`descripcion`=:descripcion,`foto`=:foto,`precio`= :precio,`categoria_Id`=:categoria_Id,`fecha`=:fecha WHERE `Id` = :Id";
+
+        $resultado = $this->cn->prepare($sql);
+
+        $_array = array(
+            ":titulo" => $_params['titulo'],
+            ":descripcion" => $_params['descripcion'],
+            ":foto" => $_params['foto'],
+            ":precio" => $_params['precio'],
+            ":categoria_Id" => $_params['categoria_Id'],
+            ":fecha" => $_params['fecha'],
+            ":Id" => $_params['Id']
+        );
+        if($resultado->execute($_array))
+            return true;
+            
+        return false;
     }
 
-    public function eliminar(){
-        
+    public function eliminar($Id){
+
+        $sql = "DELETE FROM `prenda` WHERE `Id` = :Id";
+        $resultado = $this->cn->prepare($sql);
+
+        $_array = array(
+            ":Id" => $_params['Id']
+        );
+        if($resultado->execute($_array))
+            return true;
+            
+        return false;
     }
 
     public function mostrar(){
+        $sql = "SELECT prenda.Id, titulo, descripcion, foto, nombre, precio, fecha FROM prenda
         
+        INNER JOIN categorias
+        ON prenda.categoria_Id = categorias.Id ORDER BY peliculas.Id DESC
+        ";
+        $resultado = $this->cn->prepare($sql);
+
+        if($resultado->execute())
+            return $resultado->fetchAll();
+            
+        return false;
     }
 
-    public function mostrarPorId(){
-        
+    public function mostrarPorId($Id){
+        $sql = "SELECT * FROM `prenda` WHERE `Id`=:Id";
+
+        $resultado = $this->cn->prepare($sql);
+
+        $_array = array(
+            ":Id" => $_params['Id']
+        );
+
+        if($resultado->execute($_array))
+            return $resultado->fetch();
+            
+        return false;
     }
 }
